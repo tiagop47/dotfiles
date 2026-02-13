e1aeb3a6224e1951a8c7aef2f115335c8e74a9f4
Jose Tiago <8240128@estg.ipp.pt>
2026-02-13 02:40:41 +0000

    0.0.3

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,6 +2,9 @@
 -- NEOVIM CONFIGURATION (VSCODE STYLE - STABLE)
 -- =============================================================================
 
+-- 1. BOOTSTRAP LAZY.NVIM
+vim.g.mapleader = " " -- DEFINIR ESPAÇO COMO LEADER
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
@@ -26,6 +29,23 @@ require("lazy").setup({
     config = function() 
       require('lualine').setup({ sections = { lualine_c = {{ 'filename', path = 0 }} } }) 
     end 
+  },
+
+  -- TREESITTER (Destaque de Sintaxe e Deteção de Testes)
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      local status, configs = pcall(require, "nvim-treesitter.configs")
+      if not status then return end
+      
+      configs.setup({
+        ensure_installed = { "java", "javascript", "typescript", "html", "css", "lua" },
+        sync_install = false,
+        auto_install = true,
+        highlight = { enable = true },
+      })
+    end
   },
 
   -- LSP & AUTOCOMPLETE
@@ -74,7 +94,9 @@ require("lazy").setup({
     config = function()
       require("neotest").setup({
         adapters = {
-          require("neotest-java"),
+          require("neotest-java")({
+            ignore_wrapper = false,
+          }),
           require("neotest-jest")({ jestCommand = "npm test --" }),
         }
       })
@@ -83,7 +105,18 @@ require("lazy").setup({
 
   -- UTILS
   { 'akinsho/toggleterm.nvim', config = function() require("toggleterm").setup({ open_mapping = [[<C-ç>]], direction = 'horizontal', size = 12, dir = "curr_dir" }) end },
-  { 'mg979/vim-visual-multi', init = function() vim.g.VM_maps = { ['Find Under'] = '<C-d>', ['Find Next'] = '<C-d>' } end },
+  { 
+    'mg979/vim-visual-multi', 
+    init = function() 
+      vim.g.VM_default_mappings = 0 -- Desativar padrões para não haver conflito
+      vim.g.VM_maps = {
+        ['Find Under'] = '<C-d>',
+        ['Find Next'] = '<C-d>',
+        ['Select All'] = '<C-S-L>',
+        ['Skip Region'] = '<C-x>',
+      }
+    end 
+  },
   { "karb94/neoscroll.nvim", config = function() require('neoscroll').setup() end },
 })
 
@@ -107,20 +140,26 @@ require('mason-lspconfig').setup({
 -- OPÇÕES & ATALHOS
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.opt.cursorline = true -- Destaca a linha atual, útil com números relativos
+vim.opt.cursorline = true
 vim.opt.clipboard = "unnamedplus"
 vim.opt.termguicolors = true
 vim.opt.scrolloff = 8
 
 local keymap = vim.keymap.set
 keymap('n', '<C-p>', ':Telescope find_files<CR>')
+keymap('n', '<C-S-F>', ':Telescope live_grep<CR>')
 keymap('n', '<C-S-E>', ':NvimTreeToggle<CR>')
 keymap({'n', 'i', 'v'}, '<C-s>', '<Esc>:w<CR>')
 keymap("n", "<C-k>", "<C-u>zz")
 keymap("n", "<C-j>", "<C-d>zz")
 keymap("n", "<M-S-F>", function() require("conform").format({ lsp_fallback = true }) end)
+
+-- TESTES (Agora com Espaço como Leader)
 keymap("n", "<leader>tr", function() require("neotest").run.run() end)
 keymap("n", "<leader>ts", function() require("neotest").summary.toggle() end)
+keymap("n", "<leader>to", function() require("neotest").output.open({ enter = true }) end)
+keymap("n", "<leader>gr", ":GradleRun<CR>")
+
 keymap("v", "<C-c>", '"+y')
 keymap("i", "<C-v>", '<C-r>+')
 keymap("n", "<C-v>", '"+p')
@@ -133,5 +172,6 @@ if vim.g.neovide then
     vim.g.neovide_scale_factor = 1.0
     vim.g.neovide_opacity = 0.95
     vim.keymap.set("n", "<C-=>", function() vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * 1.1 end)
+    vim.keymap.set("n", "<C-+>", function() vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * 1.1 end)
     vim.keymap.set("n", "<C-->", function() vim.g.neovide_scale_factor = vim.g.neovide_scale_factor / 1.1 end)
 end
